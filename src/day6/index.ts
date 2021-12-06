72ab60c157b1997c904332d5bebae429bd873e2b
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-05 22:35:46 -0700

    Day 6 passing

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,9 +1,59 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  const initialCounts = input[0].split(",");
-  console.log(initialCounts);
-  return;
+  const counts = input[0].split(",").map((count) => Number(count));
+
+  let counterCounts = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+  };
+  counts.forEach((count) => {
+    counterCounts[count]++;
+  });
+
+  function oneDayPasses() {
+    const resetValue = 6;
+    const newCountValue = 8;
+    const newCounterCounts = {
+      0: 0,
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+    };
+
+    for (let i = newCountValue; i >= 0; i--) {
+      if (i > 0) {
+        newCounterCounts[i - 1] = counterCounts[i];
+      } else {
+        newCounterCounts[resetValue] += counterCounts[i];
+        newCounterCounts[newCountValue] = counterCounts[i];
+      }
+    }
+    counterCounts = newCounterCounts;
+  }
+
+  for (let i = 0; i < 256; i++) {
+    oneDayPasses();
+  }
+
+  const total = Object.values(counterCounts).reduce(
+    (total: number, counts: number) => total + counts,
+    0
+  );
+  console.log(total);
+  return total;
 };
 
 export default dayFunction;
