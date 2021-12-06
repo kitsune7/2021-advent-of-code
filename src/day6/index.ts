54780a5c3156865378869d2863868e3f26f87143
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-05 22:04:53 -0700

    Prep day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -1,6 +1,8 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
+  const initialCounts = input[0].split(",");
+  console.log(initialCounts);
   return;
 };
 
