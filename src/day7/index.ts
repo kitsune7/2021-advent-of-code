421c2ce1dfcd3372cd4c8dbc07cedcb67508b0d8
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-06 22:27:55 -0700

    Completed day 7

--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,7 +1,43 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  return;
+  const positions = input[0].split(",").map((position) => Number(position));
+
+  function distance(
+    position: number,
+    b: number,
+    cost = 1,
+    totalCost = 0
+  ): number {
+    const difference = Math.abs(position - b);
+
+    if (difference !== 0) {
+      if (position > b) {
+        return distance(position - 1, b, cost + 1, totalCost + cost);
+      }
+      return distance(position + 1, b, cost + 1, totalCost + cost);
+    }
+
+    return totalCost;
+  }
+
+  const min = Math.min(...positions);
+  const max = Math.max(...positions);
+
+  let smallestFuelCost = null;
+
+  for (let i = min; i <= max; i++) {
+    const fuelCost = positions.reduce(
+      (totalCost, position) => totalCost + distance(position, i),
+      0
+    );
+    if (smallestFuelCost === null || fuelCost < smallestFuelCost) {
+      smallestFuelCost = fuelCost;
+    }
+  }
+
+  console.log(smallestFuelCost);
+  return smallestFuelCost;
 };
 
 export default dayFunction;
