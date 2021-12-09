af1d2f9b49df8e47f37272ef8d24579811f7b048
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-08 23:05:19 -0700

    Day 9, part 2

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,6 +1,12 @@
 import { DayFunction } from "../utilities";
 
+type Direction = "top" | "left" | "right" | "bottom";
+type Row = number;
+type Col = number;
+type Point = `${Row},${Col}`;
+
 const dayFunction: DayFunction = (input: string[]) => {
+  const basinSizes: number[] = [];
   const lowPoints = [];
 
   for (let row = 0; row < input.length; row++) {
@@ -14,14 +20,65 @@ const dayFunction: DayFunction = (input: string[]) => {
         col === input[row].length - 1 || number < Number(input[row][col + 1]);
 
       if (lessThanTop && lessThanBottom && lessThanLeft && lessThanRight) {
-        lowPoints.push(number);
+        lowPoints.push(`${row},${col}`);
       }
     }
   }
 
-  const result = lowPoints.reduce((total, lowPoint) => {
-    return total + lowPoint + 1;
-  }, 0);
+  const getBasinSize = (currentPoint: Point, tracked: Point[]): number => {
+    const [row, col] = currentPoint.split(",").map(Number);
+    const value = Number(input[row][col]);
+
+    if (
+      row !== 0 &&
+      !tracked.includes(`${row - 1},${col}`) &&
+      input[row - 1][col] !== "9" &&
+      value < Number(input[row - 1][col])
+    ) {
+      const point: Point = `${row - 1},${col}`;
+      tracked.push(point);
+      getBasinSize(point, tracked);
+    }
+    if (
+      row !== input.length - 1 &&
+      !tracked.includes(`${row + 1},${col}`) &&
+      input[row + 1][col] !== "9" &&
+      value < Number(input[row + 1][col])
+    ) {
+      const point: Point = `${row + 1},${col}`;
+      tracked.push(point);
+      getBasinSize(point, tracked);
+    }
+    if (
+      col !== 0 &&
+      !tracked.includes(`${row},${col - 1}`) &&
+      input[row][col - 1] !== "9" &&
+      value < Number(input[row][col - 1])
+    ) {
+      const point: Point = `${row},${col - 1}`;
+      tracked.push(point);
+      getBasinSize(point, tracked);
+    }
+    if (
+      col !== input[row].length - 1 &&
+      !tracked.includes(`${row},${col + 1}`) &&
+      input[row][col + 1] !== "9" &&
+      value < Number(input[row][col + 1])
+    ) {
+      const point: Point = `${row},${col + 1}`;
+      tracked.push(point);
+      getBasinSize(point, tracked);
+    }
+
+    return tracked.length;
+  };
+
+  for (let i = 0; i < lowPoints.length; i++) {
+    basinSizes.push(getBasinSize(lowPoints[i], [lowPoints[i]]));
+  }
+
+  basinSizes.sort((a, b) => (a < b ? -1 : 1));
+  const result = basinSizes.pop() * basinSizes.pop() * basinSizes.pop();
   console.log(result);
   return result;
 };
