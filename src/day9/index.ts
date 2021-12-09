c161083468032de18ca1b54735ad7fa28a4c09d2
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-08 22:16:30 -0700

    Day 9 part 1

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -1,7 +1,29 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  return;
+  const lowPoints = [];
+
+  for (let row = 0; row < input.length; row++) {
+    for (let col = 0; col < input[row].length; col++) {
+      const number = Number(input[row][col]);
+      const lessThanTop = row === 0 || number < Number(input[row - 1][col]);
+      const lessThanBottom =
+        row === input.length - 1 || number < Number(input[row + 1][col]);
+      const lessThanLeft = col === 0 || number < Number(input[row][col - 1]);
+      const lessThanRight =
+        col === input[row].length - 1 || number < Number(input[row][col + 1]);
+
+      if (lessThanTop && lessThanBottom && lessThanLeft && lessThanRight) {
+        lowPoints.push(number);
+      }
+    }
+  }
+
+  const result = lowPoints.reduce((total, lowPoint) => {
+    return total + lowPoint + 1;
+  }, 0);
+  console.log(result);
+  return result;
 };
 
 export default dayFunction;
