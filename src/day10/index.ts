157c2be69f5d31c9bc2b1f936e2cfde2ccbb36d2
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-09 22:42:21 -0700

    Day 10, part 2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,11 +1,11 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  const corruptedPoints = {
-    ")": 3,
-    "]": 57,
-    "}": 1197,
-    ">": 25137,
+  const autocompletePoints = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
   };
   const chunkMap = {
     "(": ")",
@@ -14,7 +14,7 @@ const dayFunction: DayFunction = (input: string[]) => {
     "<": ">",
   };
 
-  const totalPoints = input.reduce((total, line) => {
+  const incompleteLines = input.filter((line) => {
     let stack = [];
     for (let i = 0; i < line.length; i++) {
       const character = line[i];
@@ -22,14 +22,43 @@ const dayFunction: DayFunction = (input: string[]) => {
       if (Object.keys(chunkMap).includes(character)) {
         stack.push(character);
       } else if (chunkMap[stack.pop()] !== character) {
-        return total + corruptedPoints[character];
+        return false;
       }
     }
-    return total;
-  }, 0);
+    return true;
+  });
 
-  console.log(totalPoints);
-  return totalPoints;
+  const pointsPerLine = incompleteLines.map((line) => {
+    let stack = [];
+    for (let i = 0; i < line.length; i++) {
+      stack.push(line[i]);
+    }
+
+    let autocompleteString = "";
+    let closingStack = [];
+    while (stack.length) {
+      const character = stack.pop();
+      if (Object.values(chunkMap).includes(character)) {
+        closingStack.push(character);
+      }
+      if (Object.keys(chunkMap).includes(character)) {
+        if (closingStack.length) {
+          closingStack.pop();
+        } else {
+          autocompleteString += chunkMap[character];
+        }
+      }
+    }
+
+    return Array.from(autocompleteString).reduce((total, character) => {
+      return total * 5 + autocompletePoints[character];
+    }, 0);
+  });
+  const middleIndex = Math.floor(pointsPerLine.length / 2);
+  pointsPerLine.sort((a, b) => (a < b ? -1 : 1));
+
+  console.log(pointsPerLine[middleIndex]);
+  return pointsPerLine[middleIndex];
 };
 
 export default dayFunction;
