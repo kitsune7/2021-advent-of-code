abb04d291d66b691ef598b41ad4aad42715b6b40
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-09 22:18:38 -0700

    Day 10, part 1

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -1,7 +1,35 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  return;
+  const corruptedPoints = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137,
+  };
+  const chunkMap = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+    "<": ">",
+  };
+
+  const totalPoints = input.reduce((total, line) => {
+    let stack = [];
+    for (let i = 0; i < line.length; i++) {
+      const character = line[i];
+
+      if (Object.keys(chunkMap).includes(character)) {
+        stack.push(character);
+      } else if (chunkMap[stack.pop()] !== character) {
+        return total + corruptedPoints[character];
+      }
+    }
+    return total;
+  }, 0);
+
+  console.log(totalPoints);
+  return totalPoints;
 };
 
 export default dayFunction;
