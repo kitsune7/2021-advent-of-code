b6f534620dde645d14e51c60fd43540749c79706
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-12 02:03:12 -0700

    Day 12, part 1

--- src/day12/index.ts
+++ src/day12/index.ts
@@ -1,7 +1,46 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  return;
+  const nodes: Record<string, string[]> = {};
+
+  input.forEach((line) => {
+    const [nodeA, nodeB] = line.split("-");
+    if (!nodes?.[nodeA]) nodes[nodeA] = [];
+    if (!nodes?.[nodeB]) nodes[nodeB] = [];
+
+    if (!nodes[nodeA].includes(nodeB)) nodes[nodeA].push(nodeB);
+    if (!nodes[nodeB].includes(nodeA)) nodes[nodeB].push(nodeA);
+  });
+
+  const paths = new Set();
+
+  let stack = ["start"];
+  const visited = new Set();
+
+  findPath("start");
+  function findPath(currentNode: string) {
+    if (currentNode === "end") {
+      paths.add(stack.join(","));
+      return;
+    }
+
+    visited.clear();
+    stack.forEach((node) => visited.add(node));
+    const visitableNodes = nodes[currentNode].filter(
+      (node) => !visited.has(node) || node === node.toUpperCase()
+    );
+
+    visitableNodes.forEach((node) => {
+      stack.push(node);
+      findPath(node);
+      stack.pop();
+    });
+  }
+
+  console.log("PATHS:");
+  paths.forEach((path) => console.log(path));
+
+  return paths.size;
 };
 
 export default dayFunction;
