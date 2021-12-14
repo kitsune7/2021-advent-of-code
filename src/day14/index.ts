909c4dc1f8f5b7078b46ae72413eb005eaced15c
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-14 01:45:19 -0700

    Day 14, part 1

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,20 +1,44 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
-  const polymerTemplate = input[0];
+  let startingPolymerTemplate = input[0];
+
   const rules = {};
   input.slice(2).forEach((rule) => {
     const parts = rule.split(" -> ");
     rules[parts[0]] = parts[1];
   });
 
-  // get pairs
-  for (let i = 0; i < polymerTemplate.length - 1; i++) {
-    const pair = polymerTemplate.slice(i, i + 1);
-    console.log(pair);
+  const getNextStep = (polymerTemplate) => {
+    let newPolymerTemplate = polymerTemplate[0];
+
+    for (let i = 0; i < polymerTemplate.length - 1; i++) {
+      const pair = polymerTemplate.slice(i, i + 2);
+      newPolymerTemplate += `${rules[pair]}${pair[1]}`;
+    }
+
+    return newPolymerTemplate;
+  };
+
+  for (let i = 0; i < 10; i++) {
+    startingPolymerTemplate = getNextStep(startingPolymerTemplate);
   }
 
-  return;
+  const frequency = getFrequencyMap(startingPolymerTemplate);
+  const highestCount = Math.max(...Object.values(frequency));
+  const lowestCount = Math.min(...Object.values(frequency));
+
+  return highestCount - lowestCount;
+};
+
+const getFrequencyMap = (letters: string): Record<string, number> => {
+  const frequency: Record<string, number> = {};
+  Array.from(letters).forEach((letter) => {
+    if (!frequency?.[letter]) {
+      frequency[letter] = 1;
+    } else frequency[letter] += 1;
+  });
+  return frequency;
 };
 
 export default dayFunction;
