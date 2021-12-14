8cefc078c00cc5462643dcbb54eac70bf5fbebd1
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-13 22:07:06 -0700

    Prep day 14

--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,6 +1,19 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
+  const polymerTemplate = input[0];
+  const rules = {};
+  input.slice(2).forEach((rule) => {
+    const parts = rule.split(" -> ");
+    rules[parts[0]] = parts[1];
+  });
+
+  // get pairs
+  for (let i = 0; i < polymerTemplate.length - 1; i++) {
+    const pair = polymerTemplate.slice(i, i + 1);
+    console.log(pair);
+  }
+
   return;
 };
 
