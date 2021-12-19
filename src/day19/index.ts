bc47a8ac1880bb301a8ad9f7ce3cd25487035eb5
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-18 22:15:25 -0700

    Prep day 19

--- src/day19/index.ts
+++ src/day19/index.ts
@@ -1,6 +1,20 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
+  const scanners = [];
+
+  let currentScanner = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i].startsWith("---") && i !== 0) {
+      scanners.push(currentScanner);
+      currentScanner = [];
+    } else if (/^\d+,\d+/.test(input[i])) {
+      currentScanner.push(input[i].split(",").map(Number));
+    }
+  }
+
+  console.log(scanners);
+
   return;
 };
 
