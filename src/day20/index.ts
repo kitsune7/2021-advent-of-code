1b7d4b7c1e54ecc4fe61117df8cc6ef6fe9d3db3
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-20 01:07:18 -0700

    Day 19 is a struggle...

--- src/day20/index.ts
+++ src/day20/index.ts
@@ -1,6 +1,10 @@
 import { DayFunction } from "../utilities";
 
 const dayFunction: DayFunction = (input: string[]) => {
+  const imageEnhancementAlgorithm = input[0];
+  const inputImage = input.slice(2);
+
+  console.log(imageEnhancementAlgorithm);
   return;
 };
 
