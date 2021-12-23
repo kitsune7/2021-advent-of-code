bdf01eef115996612d86b84cb236ff3a2da0e218
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-23 16:45:51 -0700

    Refactored code

--- src/day19/index.ts
+++ src/day19/index.ts
@@ -1,365 +1,171 @@
-import { DayFunction, transformVector } from "../utilities";
+import { DayFunction, Matrix, transformVector } from '../utilities'
+import { transforms } from './transforms'
 
-type Coordinate = [number, number, number];
-type Scanner = Coordinate[];
-
-const transforms = [
-  [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-  ],
-  [
-    [-1, 0, 0],
-    [0, -1, 0],
-    [0, 0, 1],
-  ],
-  [
-    [1, 0, 0],
-    [0, -1, 0],
-    [0, 0, -1],
-  ],
-  [
-    [-1, 0, 0],
-    [0, 1, 0],
-    [0, 0, -1],
-  ],
-
-  [
-    [0, -1, 0],
-    [1, 0, 0],
-    [0, 0, 1],
-  ],
-  [
-    [0, 1, 0],
-    [-1, 0, 0],
-    [0, 0, 1],
-  ],
-  [
-    [0, 1, 0],
-    [1, 0, 0],
-    [0, 0, -1],
-  ],
-  [
-    [0, -1, 0],
-    [-1, 0, 0],
-    [0, 0, -1],
-  ],
-
-  [
-    [-1, 0, 0],
-    [0, 0, 1],
-    [0, 1, 0],
-  ],
-  [
-    [1, 0, 0],
-    [0, 0, -1],
-    [0, 1, 0],
-  ],
-  [
-    [1, 0, 0],
-    [0, 0, 1],
-    [0, -1, 0],
-  ],
-  [
-    [-1, 0, 0],
-    [0, 0, -1],
-    [0, -1, 0],
-  ],
-
-  [
-    [0, 0, 1],
-    [1, 0, 0],
-    [0, 1, 0],
-  ],
-  [
-    [0, 0, -1],
-    [-1, 0, 0],
-    [0, 1, 0],
-  ],
-  [
-    [0, 0, 1],
-    [-1, 0, 0],
-    [0, -1, 0],
-  ],
-  [
-    [0, 0, -1],
-    [1, 0, 0],
-    [0, -1, 0],
-  ],
-
-  [
-    [0, 1, 0],
-    [0, 0, 1],
-    [1, 0, 0],
-  ],
-  [
-    [0, -1, 0],
-    [0, 0, -1],
-    [1, 0, 0],
-  ],
-  [
-    [0, 1, 0],
-    [0, 0, -1],
-    [-1, 0, 0],
-  ],
-  [
-    [0, -1, 0],
-    [0, 0, 1],
-    [-1, 0, 0],
-  ],
-
-  [
-    [0, 0, -1],
-    [0, 1, 0],
-    [1, 0, 0],
-  ],
-  [
-    [0, 0, 1],
-    [0, -1, 0],
-    [1, 0, 0],
-  ],
-  [
-    [0, 0, 1],
-    [0, 1, 0],
-    [-1, 0, 0],
-  ],
-  [
-    [0, 0, -1],
-    [0, -1, 0],
-    [-1, 0, 0],
-  ],
-];
+type Coordinate = [number, number, number]
+type Scanner = Coordinate[]
 
 const dayFunction: DayFunction = (input: string[]) => {
-  const scanners: Scanner[] = [];
-  const fullMap = new Set<string>();
-  const minimumBeaconsOverlapping = 12;
-  let mainScannerData: { beacon: Coordinate; distances: string[] }[] = [];
+  const scanners: Scanner[] = getScannersFromInput()
+  const shiftedScanners: Scanner[] = []
+  const minimumBeaconsOverlapping = 12
 
-  void (function setup() {
-    // Add scanners from input
-    let currentScanner = [];
+  function getScannersFromInput(): Scanner[] {
+    const scanners = []
+
+    let currentScanner = []
     for (let i = 0; i < input.length; i++) {
-      if (input[i].startsWith("---") && i !== 0) {
-        scanners.push(currentScanner);
-        currentScanner = [];
+      if (input[i].startsWith('---') && i !== 0) {
+        scanners.push(currentScanner)
+        currentScanner = []
       } else if (/(-)?[0-9]+,(-)?[0-9]+/.test(input[i])) {
-        currentScanner.push(input[i].split(",").map(Number));
+        currentScanner.push(input[i].split(',').map(Number))
       }
     }
-    scanners.push(currentScanner);
+    scanners.push(currentScanner)
 
-    // Establish main scanner data
-    scanners[0].forEach((beacon, beaconIndex) => {
-      mainScannerData.push({
-        beacon,
-        distances: getBeaconDistances(scanners[0], beaconIndex),
-      });
-    });
+    return scanners
+  }
 
-    // Set initial beacons on full map
-    scanners[0].forEach((beacon) => fullMap.add(beacon.toString()));
-  })();
+  function getShiftedScanners() {
+    const shiftedScannerIndexes: number[] = []
 
-  function getBeaconMatch(
-    scanner: Coordinate[],
-    beaconIndex: number
-  ): [Coordinate, Coordinate] | null {
-    const distances = getBeaconDistances(scanner, beaconIndex);
+    while (shiftedScannerIndexes.length < scanners.length) {
+      for (let i = 0; i < scanners.length; i++) {
+        if (shiftedScannerIndexes.includes(i)) continue
+        const scanner = scanners[i]
 
-    const match = mainScannerData.find((mainBeacon) => {
-      let count = 0;
-      for (let i = 0; i < distances.length; i++) {
-        if (mainBeacon.distances.includes(distances[i])) count++;
+        console.log(`Adding scanner ${i} to map`)
+        if (getShiftedScanner(scanner, i)) {
+          shiftedScannerIndexes.push(i)
+        } else {
+          console.log(`Failed to add scanner ${i}`)
+        }
       }
-
-      return count >= minimumBeaconsOverlapping - 1;
-    });
-
-    return match ? [scanner[beaconIndex], match.beacon] : null;
+    }
   }
 
-  function getScannerMatches(scanner: Coordinate[]): Array<Coordinate[]> {
-    const matches = [];
+  function getShiftedScanner(scanner: Scanner, index: number): boolean {
+    // Use scanner 0 as the main point of reference
+    if (!shiftedScanners.length) {
+      shiftedScanners.push(scanner)
+      return
+    }
 
-    scanner.forEach((beacon, beaconIndex) => {
-      const match = getBeaconMatch(scanner, beaconIndex);
-      if (match) {
-        matches.push(match);
+    shiftedScanners.forEach((shiftedScanner, shiftedScannerIndex) => {
+      console.log(
+        `Checking scanner ${index} for matches against (shifted) scanner ${shiftedScannerIndex}`
+      )
+
+      for (let i = 0; i < transforms.length; i++) {
+        const transformed = getTransformedScanner(scanner, transforms[i])
+        // Order here matters. Try switching the order of the two is the orientation calculation is wrong
+        const matches = getScannerMatches(transformed, shiftedScanner)
+
+        if (matches.length) {
+          if (isCorrectOrientation(matches)) {
+            shiftedScanners.push(shiftScanner(calculateOffset(matches[0]), transformed))
+            return true
+          }
+        }
       }
-    });
+    })
 
-    return matches;
+    return false
   }
 
-  function recalculateDistances() {
-    const mainBeacons = mainScannerData.map((item) => item.beacon);
-    mainScannerData = mainScannerData.map((item, index) => ({
-      beacon: item.beacon,
-      distances: getBeaconDistances(mainBeacons, index),
-    }));
+  function getTransformedScanner(scanner: Scanner, transformMatrix: Matrix<number>) {
+    const transformedScanner = []
+
+    scanner.forEach((beacon) => {
+      transformedScanner.push(transformVector(beacon, transformMatrix) as Coordinate)
+    })
+
+    return transformedScanner
   }
 
-  function addShiftedBeaconsToMap(
-    matches: Coordinate[][],
-    scannerIndex: number
-  ) {
-    const offset = calculateOffset(matches[0]);
-    const shiftedScanner = shiftScanner(offset, scanners[scannerIndex]);
-    shiftedScanner.forEach((beacon) => {
-      fullMap.add(beacon.toString());
+  // I could probably optimize this by exiting as soon as I have 2 matches because that's all that's needed to check the
+  // orientation.
+  function getScannerMatches(
+    scannerA: Scanner,
+    scannerB: Scanner
+  ): Array<[Coordinate, Coordinate]> {
+    const matches = []
 
-      if (
-        !mainScannerData.find(
-          (mainBeacon) => mainBeacon.toString() === beacon.toString()
+    scannerA.forEach((beaconA, beaconAIndex) => {
+      scannerB.forEach((beaconB, beaconBIndex) => {
+        const beaconADistances = getBeaconDistances(scannerA, beaconAIndex)
+        const beaconBDistances = getBeaconDistances(scannerB, beaconBIndex)
+        const matchingDistanceCount = countCommonDistances(beaconADistances, beaconBDistances)
+
+        if (matchingDistanceCount >= minimumBeaconsOverlapping - 1) {
+          matches.push([beaconA, beaconB])
+        }
+      })
+    })
+
+    return matches
+  }
+
+  function getBeaconDistances(scanner: Coordinate[], beaconIndex: number): number[] {
+    const distances = []
+
+    for (let i = 0; i < scanner.length; i++) {
+      if (scanner[beaconIndex].toString() === scanner[i].toString()) continue
+      distances.push(
+        Math.sqrt(
+          (scanner[beaconIndex][0] - scanner[i][0]) ** 2 +
+            (scanner[beaconIndex][1] - scanner[i][1]) ** 2 +
+            (scanner[beaconIndex][2] - scanner[i][2]) ** 2
         )
-      ) {
-        mainScannerData.push({
-          beacon,
-          distances: [],
-        });
-        recalculateDistances();
-      }
-    });
+      )
+    }
+
+    return distances
   }
 
-  function transformScanner(scanner: Scanner, transformIndex: number) {
-    scanner.forEach((beacon, beaconIndex) => {
-      scanner[beaconIndex] = transformVector(
-        beacon,
-        transforms[transformIndex]
-      ) as Coordinate;
-    });
-  }
+  function countCommonDistances(distancesA: number[], distancesB: number[]): number {
+    let count = 0
 
-  function getTransformedScanner(scanner: Scanner, transformIndex: number) {
-    const transformedScanner = [];
+    for (let i = 0; i < distancesA.length; i++) {
+      for (let j = 0; j < distancesB.length; j++) {
+        const distanceA = distancesA[i]
+        const distanceB = distancesB[j]
 
-    scanner.forEach((beacon, beaconIndex) => {
-      transformedScanner.push(
-        transformVector(beacon, transforms[transformIndex]) as Coordinate
-      );
-    });
-    console.log(transformedScanner);
-
-    return transformedScanner;
-  }
-
-  function addScannerBeaconsToMap(scanner: Scanner, index: number): boolean {
-    let matches = getScannerMatches(scanner);
-
-    for (let i = 0; i < transforms.length; i++) {
-      // This code assumes that the first transform is the identity matrix
-      if (i > 0) {
-        const transformed = getTransformedScanner(scanner, i);
-        matches = getScannerMatches(transformed);
-        console.log(matches.length);
-        if (matches.length) transformScanner(scanner, i);
-      }
-
-      if (matches.length && isCorrectOrientation(matches)) {
-        console.log(
-          `isCorrectOrientation(matches)`,
-          isCorrectOrientation(matches)
-        );
-        addShiftedBeaconsToMap(matches, index + 1);
-        return true;
+        // I could try Math.floor or Math.ceil if there's some kind of odd JS error due to floating point imprecision
+        if (distanceA === distanceB) {
+          count++
+        }
       }
     }
 
-    return false;
+    return count
   }
 
-  let retryIndexes = [];
-  scanners.slice(1).forEach((scanner, index) => {
-    console.log(`Adding scanner ${index + 1} to map`);
-    if (!addScannerBeaconsToMap(scanner, index)) {
-      console.log(`Scanner ${index + 1} couldn't be added`);
-      retryIndexes.push(index);
-    }
-  });
-  while (retryIndexes.length) {
-    const successful = [];
-    for (let i = 0; i < retryIndexes.length; i++) {
-      console.log(`Retrying adding scanner ${retryIndexes[i] + 1} to map`);
-      if (addScannerBeaconsToMap(scanners[retryIndexes[i]], retryIndexes[i])) {
-        successful.push(retryIndexes[i]);
-      }
-    }
-    retryIndexes = retryIndexes.filter((index) => !successful.includes(index));
-    console.log(fullMap.size);
+  function isCorrectOrientation(matches: Array<[Coordinate, Coordinate]>): boolean {
+    const firstOffset = calculateOffset(matches[0]).toString()
+    return matches.slice(1).every((match) => calculateOffset(match).toString() === firstOffset)
   }
 
-  console.log(fullMap);
-
-  return fullMap.size;
-};
-
-export default dayFunction;
-
-function getBeaconDistances(
-  scanner: Coordinate[],
-  beaconIndex: number
-): string[] {
-  const distances = [];
-
-  for (let i = 0; i < scanner.length; i++) {
-    if (scanner[beaconIndex].toString() === scanner[i].toString()) continue;
-
-    const absoluteDistance = Math.sqrt(
-      (scanner[beaconIndex][0] - scanner[i][0]) ** 2 +
-        (scanner[beaconIndex][1] - scanner[i][1]) ** 2 +
-        (scanner[beaconIndex][2] - scanner[i][2]) ** 2
-    );
-
-    const xDistance = Math.abs(scanner[beaconIndex][0] - scanner[i][0]);
-    const yDistance = Math.abs(scanner[beaconIndex][1] - scanner[i][1]);
-    const zDistance = Math.abs(scanner[beaconIndex][2] - scanner[i][2]);
-    const distanceString = `${absoluteDistance}`;
-
-    distances.push(distanceString);
+  function calculateOffset(match: Coordinate[]): Coordinate {
+    return [match[1][0] - match[0][0], match[1][1] - match[0][1], match[1][2] - match[0][2]]
   }
 
-  return distances.sort();
+  function shiftScanner(offset: Coordinate, scanner: Scanner): Scanner {
+    return scanner.map((beacon) => beacon.map((num, index) => num + offset[index])) as Scanner
+  }
+
+  function getUniqueBeaconCount(): number {
+    const fullMap = new Set<string>()
+
+    shiftedScanners.forEach((scanner) =>
+      scanner.forEach((beacon) => fullMap.add(beacon.toString()))
+    )
+
+    return fullMap.size
+  }
+
+  getShiftedScanners()
+  return getUniqueBeaconCount()
 }
 
-function isCorrectOrientation(matches: Coordinate[][]): boolean {
-  const firstOffset = calculateOffset(matches[0]).toString();
-  return matches
-    .slice(1)
-    .every((match) => calculateOffset(match).toString() === firstOffset);
-}
-
-function calculateOffset(match: Coordinate[]): Coordinate {
-  return [
-    match[1][0] - match[0][0],
-    match[1][1] - match[0][1],
-    match[1][2] - match[0][2],
-  ];
-}
-
-function shiftScanner(offset: Coordinate, scanner: Coordinate[]): Coordinate[] {
-  return scanner.map((beacon) =>
-    beacon.map((num, index) => num + offset[index])
-  ) as Coordinate[];
-}
-
-// const absoluteDistance = Math.sqrt(
-//   (scanner[j][0] - scanner[i][0]) ** 2 +
-//   (scanner[j][1] - scanner[i][1]) ** 2 +
-//   (scanner[j][2] - scanner[i][2]) ** 2
-// );
-
-// const totalBeacons = Object.values(distances).reduce(
-//   (total, value) => total + (value > 1 ? 1 : 0),
-//   0
-// );
-
-// console.log(
-//   Object.fromEntries(
-//     Object.entries(distances).filter(([_, value]) => value > 1)
-//   )
-// );
-
-// return totalBeacons;
+export default dayFunction
