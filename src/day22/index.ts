0f375a452170142d5ba8cd6c7635e08c45fca4da
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-25 00:52:25 -0700

    Passed minimal test sample

--- src/day22/index.ts
+++ src/day22/index.ts
@@ -8,17 +8,76 @@ type Instruction = {
 }
 
 const dayFunction: DayFunction = (input: string[]) => {
-  const instructions: Instruction[] = input.map((line) => ({
-    on: line.startsWith('on'),
-    range: line.split(',').map((rangeString) =>
-      rangeString
-        .replace(/(on|off| |[xyz]=)/g, '')
-        .split('..')
-        .map(Number)
-    ) as Range,
-  }))
-  const countedRanges: Range[] = []
+  const instructions: Instruction[] = input
+    .map((line) => ({
+      on: line.startsWith('on'),
+      range: line.split(',').map((rangeString) =>
+        rangeString
+          .replace(/(on|off| |[xyz]=)/g, '')
+          .split('..')
+          .map(Number)
+      ) as Range,
+    }))
+    .filter((instruction) =>
+      instruction.range.every((pair) => pair.every((num) => num >= -50 && num <= 50))
+    )
+    .reverse()
   let onCount = 0
+  const counted: Range[] = []
+
+  function countCubes() {
+    for (let i = 0; i < instructions.length; i++) {
+      const instruction = instructions[i]
+      const rangeCount = countRange(instruction.range)
+
+      if (instruction.on) {
+        onCount += rangeCount
+        console.log(`Adding ${rangeCount} to onCount for ${instruction.range}`)
+
+        let overlaps = []
+        for (let j = 0; j < counted.length; j++) {
+          const overlapRange = findRangeOverlap(instruction.range, instructions[j].range)
+          if (overlapRange) {
+            overlaps.push(overlapRange)
+            console.log(`Found overlap of ${countRange(overlapRange)}. ${overlapRange}`)
+            onCount -= countRange(overlapRange)
+          }
+        }
+
+        for (let i = 0; i < overlaps.length; i++) {
+          for (let j = i + 1; j < overlaps.length; j++) {
+            const overlapRange = findRangeOverlap(overlaps[i], overlaps[j])
+            if (overlapRange) {
+              onCount += countRange(overlapRange)
+            }
+          }
+        }
+      }
+
+      counted.push(instruction.range)
+    }
+  }
+
+  function countRange(range: Range): number {
+    if (!range) return 0
+    return (
+      (range[0][1] - range[0][0] + 1) *
+      (range[1][1] - range[1][0] + 1) *
+      (range[2][1] - range[2][0] + 1)
+    )
+  }
+
+  function findRangeOverlap(rangeA: Range, rangeB: Range): Range | null {
+    const xOverlap = findPairOverlap(rangeA[0], rangeB[0])
+    const yOverlap = findPairOverlap(rangeA[1], rangeB[1])
+    const zOverlap = findPairOverlap(rangeA[1], rangeB[1])
+
+    if (xOverlap && yOverlap && zOverlap) {
+      return [xOverlap, yOverlap, zOverlap]
+    }
+
+    return null
+  }
 
   function findPairOverlap(pairA: Pair, pairB: Pair): Pair | null {
     if (pairB[0] >= pairA[0] && pairB[1] <= pairA[1]) {
@@ -36,150 +95,7 @@ const dayFunction: DayFunction = (input: string[]) => {
     return null
   }
 
-  function findRangeOverlap(rangeA: Range, rangeB: Range): Range | null {
-    const xOverlap = findPairOverlap(rangeA[0], rangeB[0])
-    const yOverlap = findPairOverlap(rangeA[1], rangeB[1])
-    const zOverlap = findPairOverlap(rangeA[1], rangeB[1])
-
-    if (xOverlap && yOverlap && zOverlap) {
-      return [xOverlap, yOverlap, zOverlap]
-    }
-
-    return null
-  }
-
-  function countRange(range: Range): number {
-    if (!range) return 0
-    return (
-      (range[0][1] - range[0][0] + 1) *
-      (range[1][1] - range[1][0] + 1) *
-      (range[2][1] - range[2][0] + 1)
-    )
-  }
-
-  function splitRange(range: Range, overlapRange: Range): Range[] {
-    const ranges = []
-
-    const xSplit = splitAxis(range[0], overlapRange[0])
-    const xSafeRanges = xSplit.safeRanges.map((safeXRange) => [safeXRange, range[1], range[2]])
-    ranges.push(...xSafeRanges)
-
-    const ySplit = splitAxis(range[1], overlapRange[1])
-    const ySafeRanges = ySplit.safeRanges.map((safeYRange) => [
-      xSplit.overlapRange ?? range[0],
-      safeYRange,
-      range[2],
-    ])
-    ranges.push(...ySafeRanges)
-
-    const zSplit = splitAxis(range[2], overlapRange[2])
-    const zSafeRanges = zSplit.safeRanges.map((safeZRange) => [
-      xSplit.overlapRange ?? range[0],
-      ySplit.overlapRange ?? range[1],
-      safeZRange,
-    ])
-    ranges.push(...zSafeRanges)
-
-    return ranges
-  }
-
-  function splitAxis(
-    axisRange: Pair,
-    axisOverlap: Pair
-  ): {
-    safeRanges: Array<Pair>
-    overlapRange: Pair | null
-  } {
-    const split = {
-      safeRanges: [axisRange],
-      overlapRange: null,
-    }
-
-    if (axisOverlap[0] > axisRange[0] && axisOverlap[1] < axisRange[1]) {
-      split.safeRanges = [
-        [axisRange[0], axisOverlap[0] - 1],
-        [axisOverlap[1] + 1, axisRange[1]],
-      ]
-      split.overlapRange = axisOverlap
-    } else if (axisOverlap[0] <= axisRange[0] && axisOverlap[1] < axisRange[1]) {
-      split.safeRanges = [[axisOverlap[1] + 1, axisRange[1]]]
-      split.overlapRange = [axisRange[0], axisOverlap[1]]
-    } else if (axisOverlap[0] > axisRange[0] && axisOverlap[1] >= axisRange[1]) {
-      split.safeRanges = [[axisRange[0], axisOverlap[0] - 1]]
-      split.overlapRange = [axisOverlap[0], axisRange[1]]
-    }
-
-    return split
-  }
-
-  console.log(`split axis`, splitAxis([11, 13], [11, 12]))
-
-  function handleOverlap({
-    countedRange,
-    overlapCount,
-    range,
-    rangeCount,
-    rangeIndex,
-    rangeList,
-  }): number {
-    const overlapRange = findRangeOverlap(range, countedRange)
-    console.log(range, `overlaps ${countRange(overlapRange)} points`)
-    console.log(`overlap range`, overlapRange)
-
-    if (overlapRange) {
-      overlapCount += countRange(overlapRange)
-      if (overlapCount !== rangeCount) {
-        const split = splitRange(range, overlapRange)
-        // console.log('split', split)
-        rangeList[rangeIndex] = null
-        rangeList.push(...split)
-      }
-    }
-
-    return overlapCount
-  }
-
-  for (let i = 0; i < instructions.length; i++) {
-    const instruction = instructions[i]
-    const rangeCount = countRange(instruction.range)
-
-    if (instruction.on) {
-      onCount += rangeCount
-    }
-
-    let overlapCount = 0
-    let rangeList = [instruction.range]
-    for (let j = 0; j < countedRanges.length; j++) {
-      rangeList.forEach((range, rangeIndex) => {
-        overlapCount = handleOverlap({
-          countedRange: countedRanges[j],
-          overlapCount,
-          range,
-          rangeCount,
-          rangeIndex,
-          rangeList,
-        })
-      })
-      rangeList = rangeList.filter((range) => !!range)
-    }
-
-    if (!countedRanges.length) {
-      countedRanges.push(instruction.range)
-    }
-
-    if (overlapCount) {
-      onCount -= overlapCount
-      if (rangeList.length) {
-        console.log('Adding everything in', rangeList, 'to countedRanges')
-        countedRanges.push(...rangeList)
-      } else if (overlapCount !== rangeCount) {
-        countedRanges.push(instruction.range)
-      }
-    }
-
-    console.log(`countedRanges`, countedRanges)
-  }
-
+  countCubes()
   return onCount
 }
 
