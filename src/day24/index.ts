a7697dca2e32262db401734195b3ff54ed227b3e
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-31 17:51:57 -0700

    Setup day 24

---
+++ src/day24/index.ts
@@ -0,0 +1,12 @@
+import { DayFunction } from '../utilities'
+
+const dayFunction: DayFunction = (instructions: string[]) => {
+  let w = 0
+  let x = 0
+  let y = 0
+  let z = 0
+
+  return ''
+}
+
+export default dayFunction
