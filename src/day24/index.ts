8a0abbe74765bed2ea82449c01177d3d2a67ef95
Christopher Bradshaw <chris.kofox@gmail.com>
2021-12-31 20:52:48 -0700

    Brute-force method

--- src/day24/index.ts
+++ src/day24/index.ts
@@ -1,12 +1,93 @@
 import { DayFunction } from '../utilities'
 
-const dayFunction: DayFunction = (instructions: string[]) => {
-  let w = 0
-  let x = 0
-  let y = 0
-  let z = 0
+type Variable = 'w' | 'x' | 'y' | 'z'
+type State = Record<Variable, number> & { inputs: number[] }
 
-  return ''
+const dayFunction: DayFunction = (instructions: string[]) => {
+  const variables = ['w', 'x', 'y', 'z']
+  const commandToFunction = {
+    add: add,
+    mul: mul,
+    div: div,
+    mod: mod,
+    eql: eql,
+  }
+
+  function inp(state: State, a: Variable) {
+    state[a] = state.inputs.pop()
+  }
+
+  function evaluate(state: State, data: string): number {
+    if (variables.includes(data)) {
+      return state[data]
+    }
+    return Number(data)
+  }
+
+  function add(state: State, a: Variable, b: string) {
+    state[a] = state[a] + evaluate(state, b)
+  }
+
+  function mul(state: State, a: Variable, b: string) {
+    state[a] = state[a] * evaluate(state, b)
+  }
+
+  function div(state: State, a: Variable, b: string) {
+    state[a] = Math.floor(state[a] / evaluate(state, b))
+  }
+
+  function mod(state: State, a: Variable, b: string) {
+    state[a] = state[a] % evaluate(state, b)
+  }
+
+  function eql(state: State, a: Variable, b: string) {
+    state[a] = state[a] === evaluate(state, b) ? 1 : 0
+  }
+
+  function isValidModelNumber(modelNumber: number): boolean {
+    const modelNumberInputs = modelNumber.toString().split('').map(Number)
+
+    if (modelNumberInputs.length !== 14 || modelNumberInputs.some((num) => num === 0)) {
+      return false
+    }
+
+    const state: State = {
+      inputs: modelNumberInputs.reverse(),
+      w: 0,
+      x: 0,
+      y: 0,
+      z: 0,
+    }
+
+    for (let i = 0; i < instructions.length; i++) {
+      runInstruction(instructions[i], state)
+    }
+
+    return !state.z
+  }
+
+  function runInstruction(instruction: string, state: State) {
+    if (instruction.startsWith('inp')) {
+      inp(state, instruction.split(' ')[1] as Variable)
+    } else {
+      const [command, a, b] = instruction.split(' ')
+      commandToFunction[command](state, a, b)
+    }
+  }
+
+  for (let modelNumber = 99999999999999; modelNumber >= 11111111111111; modelNumber--) {
+    let modelNumberString = modelNumber.toString()
+    if (modelNumberString.includes('0')) {
+      modelNumberString.replace(/0/g, '9')
+      continue
+    }
+
+    if (isValidModelNumber(modelNumber)) {
+      return modelNumber
+    }
+  }
+
+  return 11111111111111
 }
 
 export default dayFunction
